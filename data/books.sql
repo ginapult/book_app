303c35a96ca9efa3642387657a787765249ced32
sajafisher29 <sajafisher@gmail.com>
2019-07-25 12:18:20 -0700

    Activated database and table

--- data/books.sql
+++ data/books.sql
@@ -4,7 +4,7 @@ CREATE TABLE books(
   id SERIAL PRIMARY KEY,
   title VARCHAR (255),
   author VARCHAR (255),
-  isbn VARCHAR (15),
+  isbn VARCHAR (255),
   image_url VARCHAR (255),
   description TEXT,
   bookshelf VARCHAR (255)
